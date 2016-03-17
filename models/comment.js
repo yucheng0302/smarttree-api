b0aa4ca29f64d59ba8cd88b12ffd65bb0413dea8
Dao <kdao@kdao01sjl.local>
2016-03-16 20:10:45 -0700

    Finish RESTFUL service for smart tree registration

--- models/comment.js
+++ models/comment.js
@@ -6,5 +6,51 @@ var util = require('util');
 var Model = require('./base');
 
 var CommentModel = function() {};
+util.inherits(CommentModel, Model);
+
+CommentModel.prototype.comments = function(params, success, fail) {
+  console.log('>>>>>>> Get comments in DB <<<<<<');
+  var self = this;
+  self.init([], success, fail);
+  var db = self.db();
+  var logger = self.logger();
+  var sql = db.prepare('SELECT Comments.comment, Users.userName FROM Comments INNER JOIN Users ON Comments.userId = Users.userId');
+
+  db.query(sql())
+    .on('result', function(res) {
+      res.on('data', function onRow(row) {
+        logger.debug({ 'row': row });
+        self.addResult(row);
+      })
+      .on('error', self.queryError.bind(self))
+      .on('end', self.queryEnd.bind(self));
+    })
+    .on('error', self.resultError.bind(self))
+    .on('end', self.resultEnd.bind(self));
+
+  db.end();
+};
+
+CommentModel.prototype.add = function(params, success, fail) {
+  var self = this;
+  self.init({}, success, fail);
+  var db = self.db();
+  var logger = self.logger();
+  console.log(params);
+  var sql = db.prepare('INSERT INTO Comments (userId, comment) VALUES (:userId, :comment)');
+  db.query(sql({userId: params.userId, comment: params.comment }))
+    .on('result', function(res) {
+      res.on('data', function onRow(row) {
+        logger.debug({ 'row': row });
+        self.setResult(row);
+      })
+      .on('error', self.queryError.bind(self))
+      .on('end', self.queryEnd.bind(self));
+    })
+    .on('error', self.resultError.bind(self))
+    .on('end', self.resultEnd.bind(self));
+
+  db.end();
+};
 
 module.exports = CommentModel;
